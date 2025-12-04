270ceb852735ae8ac0bdcb1a6285dd2291dd01f7
RAVALI ALIGETI <143004098+Ravali-951@users.noreply.github.com>
2025-12-05 05:09:49 +0530

    add aws provider config

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -1,1 +1,16 @@
-# Terraform code will go here
+# main.tf - basic Terraform + AWS setup
+
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1" # Mumbai region – change if you want another region
+}
