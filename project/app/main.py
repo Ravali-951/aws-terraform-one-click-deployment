cd23d201ed722d166b233142f99d8a39185e17fe
RAVALI ALIGETI <143004098+Ravali-951@users.noreply.github.com>
2025-12-05 02:28:54 +0530

    Create main.py

---
+++ project/app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello!"
+
+@app.route("/health")
+def health():
+    return "ok"
+
+app.run(host="0.0.0.0", port=8080)
