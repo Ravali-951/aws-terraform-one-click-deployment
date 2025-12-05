eb790197de1b32a6cc66bc9953313173fd5762e1
RAVALI ALIGETI <143004098+Ravali-951@users.noreply.github.com>
2025-12-05 08:47:44 +0530

    add alb dns output

--- project/terraform/outputs.tf
+++ project/terraform/outputs.tf
@@ -1,1 +1,6 @@
-# Output values such as ALB DNS will be added here
+# outputs.tf - show ALB DNS after apply
+
+output "alb_dns" {
+  description = "DNS name of the application load balancer"
+  value       = aws_lb.app_alb.dns_name
+}
